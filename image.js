a38fc0c286d865bb39c9e3f658c915c161f9d70e
zhangxinmeng <zhangxinmeng@ainirobot.com>
2019-08-18 15:15:14 +0800

    load picture: gif and webp

--- image.js
+++ image.js
@@ -19,6 +19,8 @@ export class ImageScreen extends React.Component {
                 <Image
                     source={require('./img/baihui.jpg')}/>
                 <Image style={{width:50,height:50}} source={{uri:'https://facebook.github.io/react-native/docs/assets/favicon.png'}}/>
+                {/*<Image style={{width:50,height:50}} source={{uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566121244293&di=c6beab3e0c6b8c559d9a4789510f488b&imgtype=0&src=http%3A%2F%2Fgss0.bdstatic.com%2F5eR1dDebRNRTm2_p8IuM_a%2Fres%2Fr%2Fimage%2F2014-06-10%2F406f1dfaa27b2c52f508533d6828c60c.gif'}}/>*/}
+                <Image style={{width:50,height:50}} source={require('./img/timg.gif')}/>
             </View>
         );
     }
